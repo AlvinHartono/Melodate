c92c4b7117ee6b638d3bccf3731c9530682aae32
Bryan Nathaniel <bryan.nathaniel73@gmail.com>
2024-11-21 01:02:02 +0700

    Make The Card Design

---
+++ app/src/main/java/com/example/melodate/ui/dashboard/CardData.kt
@@ -0,0 +1,12 @@
+package com.example.melodate.ui.dashboard
+
+data class CardData(
+    val name: String,
+    val song_title : String,
+    val artist_name : String,
+    val description: String,
+    val image: Int,
+    val musicInterest: String,
+    val location: String
+)
+
